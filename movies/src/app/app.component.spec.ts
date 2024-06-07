b7cb790b21295c84839c553d0257befa1fdbe8a5
GiampyLux <giampaolopaladino01@gmail.com>
2024-06-07 12:03:35 +0200

    commit di restart

--- movies/src/app/app.component.spec.ts
+++ movies/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'login-films'`, () => {
+  it(`should have as title 'movies'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('login-films');
+    expect(app.title).toEqual('movies');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, login-films');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, movies');
   });
 });
